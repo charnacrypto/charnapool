702b9b1ab23d2c32d277a80e834a3da6699548f2
Mine Coins <mine.coins@yandex.com>
2016-12-20 11:05:28 +0200

    initial netwok page

--- website/config.js
+++ website/config.js
@@ -10,6 +10,8 @@ var email = "support@poolhost.com";
 
 var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
 
+var networkStat = [["xci.mypool.name", "http://xci.mypool.name:42004"], ["democats.org", "http://pool3.democats.org:7713"], ["gorillapool.org", "http://gorillapool.org:8117"]];
+
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
 var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
