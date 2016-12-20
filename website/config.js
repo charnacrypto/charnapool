e310424d810952772258400e66de0d35e98208de
Mine Coins <mine.coins@yandex.com>
2016-12-20 14:56:11 +0200

    add predefined pools for network page

--- website/config.js
+++ website/config.js
@@ -10,8 +10,6 @@ var email = "support@poolhost.com";
 
 var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
 
-var networkStat = [["xci.mypool.name", "http://xci.mypool.name:42004"], ["democats.org", "http://pool3.democats.org:7713"], ["gorillapool.org", "http://gorillapool.org:8117"]];
-
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
 var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
@@ -19,3 +17,33 @@ var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
 var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
 
 var themeCss = "themes/default-theme.css";
+
+var networkStat = {
+    "bic": [
+        ["bic.mypool.name", "http://bic.mypool.name:11886"],
+        ["democats.org", "http://pool2.democats.org:7663"],
+        ["bikercoin.acidminer.org", "http://bikercoin.acidminer.org:8119"],
+        ["bikercoin.org", "http://bikercoin.org:8117"]
+    ],
+    "bip": [
+        ["bip.mypool.name", "http://bip.mypool.name:18874"],
+        ["democats.org", "http://pool.democats.org:7693"],
+        ["bip.cryptonotepool.com", "http://5.189.135.137:8121"]
+    ],
+    "coal": [
+        ["coal.mypool.name", "http://coal.mypool.name:7704"],
+        ["democats.org", "http://pool.democats.org:7703"]
+    ],
+    "dsh": [
+        ["dsh.mypool.name", "http://dsh.mypool.name:29084"],
+        ["democats.org", "http://pool.democats.org:7613"]
+    ],
+    "xci": [
+        ["xci.mypool.name", "http://xci.mypool.name:42004"],
+        ["democats.org", "http://pool3.democats.org:7713"],
+        ["gorillapool.org", "http://gorillapool.org:8117"],
+        ["xci.acidminer.org", "http://xci.acidminer.org:8117"],
+        ["xci.cryptonotepool.com", "http://5.189.135.137:8119"],
+        ["xci.ms-pool.net.ua", "http://xci.ms-pool.net.ua:8117"]
+    ]
+};
