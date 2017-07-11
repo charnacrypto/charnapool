7e5b0a446bea7dd387ac5ff243f4d92b6100dd0b
tnga <devtnga@gmail.com>
2017-07-11 16:05:26 +0100

    use configuration sample for more flexibility

---
+++ website/config.sample.js
@@ -0,0 +1,74 @@
+var api = "http://localhost:8113";
+
+var api_blockexplorer = "http://localhost:1118";
+
+var poolHost = "poolhost.com";
+
+var irc = "irc.freenode.net/#poolhost";
+
+var email = "support@poolhost.com";
+
+var cryptonatorWidget = ["XMR-BTC", "XMR-USD", "XMR-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+
+var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+
+var themeCss = "themes/default-theme.css";
+
+var networkStat = {
+    "bcn": [
+        ["bcn.mypool.name", "http://bcn.mypool.name:8084"],
+        ["democats.org", "http://pool.democats.org:7603"]
+    ],
+    "bic": [
+        ["bic.mypool.name", "http://bic.mypool.name:11886"],
+        ["democats.org", "http://pool2.democats.org:7663"],
+        ["bikercoin.acidminer.org", "http://bikercoin.acidminer.org:8119"],
+        ["pool.bikercoin.org", "http://pool.bikercoin.org:8117"]
+    ],
+    "bip": [
+        ["bip.mypool.name", "http://bip.mypool.name:18874"],
+        ["democats.org", "http://pool.democats.org:7693"],
+        ["bip.cryptonotepool.com", "http://5.189.135.137:8121"],
+        ["bip.ms-pool.net.ua", "http://bip.ms-pool.net.ua:8117"],
+        ["bip.crypto-coins.club", "http://5.157.83.120:8118"]
+    ],
+    "coal": [
+        ["coal.mypool.name", "http://coal.mypool.name:7704"],
+        ["democats.org", "http://pool.democats.org:7703"]
+    ],
+    "dsh": [
+        ["dsh.mypool.name", "http://dsh.mypool.name:29084"],
+        ["democats.org", "http://pool.democats.org:7613"]
+    ],
+    "fcn": [
+        ["fcn.mypool.name", "http://fcn.mypool.name:24084"]
+    ],
+    "krb": [
+        ["krb.mypool.name", "http://krb.mypool.name:32351"],
+        ["democats.org", "http://pool2.democats.org:7673"],
+        ["pool.karbowanec.com", "http://pool.karbowanec.com:8117"],
+        ["pool2.karbowanec.com", "http://pool2.karbowanec.com:8117"],
+        ["krb.tfoto.com.ua", "http://krb.tfoto.com.ua:8117"],
+        ["krb.sberex.com", "http://krb.sberex.com:7006"],
+        ["amazon.sberex.com", "http://amazon.sberex.com:7006"],
+        ["krb.crypto-coins.club", "http://krb.crypto-coins.club:8118"],
+        ["xcrypto.org", "http://xcrypto.org:8117"],
+        ["krb.cryptonotepool.com", "http://5.189.135.137:8618"]
+    ],
+    "qcn": [
+        ["qcn.mypool.name", "http://qcn.mypool.name:23084"]
+    ],
+    "xci": [
+        ["xci.mypool.name", "http://xci.mypool.name:42004"],
+        ["democats.org", "http://pool3.democats.org:7713"],
+        ["gorillapool.org", "http://gorillapool.org:8117"],
+        ["xci.acidminer.org", "http://xci.acidminer.org:8117"],
+        ["xci.cryptonotepool.com", "http://5.189.135.137:8119"],
+        ["xci.ms-pool.net.ua", "http://xci.ms-pool.net.ua:8117"],
+        ["xci.crypto-coins.club", "http://xci.crypto-coins.club:8118"]
+    ]
+};
